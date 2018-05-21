bdc0d56edb4e33790e9f6bc8273fb59b58fc451c
cavidann <cavi98@icloud.com>
2018-05-21 23:06:36 +0600

    loader updated

--- script/main.js
+++ script/main.js
@@ -1,16 +1,22 @@
+$(window).on("load", function () {
 
-$(document).ready(function(){
-     $(".fa-bars").click(function(){
+    $("#loading").fadeOut(1000)
+})
+$(document).ready(function () {
+    $(".fa-bars").click(function () {
         $(".resp-menu").toggle("slow");
     });
-    setTimeout(function(){ 
-        $("#loading").fadeOut()
-    }, 2000);
-    ;
+
+
+    // setTimeout(function(){ 
+    //     $("#loading").fadeOut()
+    // }, 1000);
+    // ;
     $('.three-slide').owlCarousel({
         items: 3,
         loop: true,
-        nav:true,
+        autoplay: true,
+        nav: true,
         margin: 30,
         autoplayTimeout: 1000,
         autoplayHoverPause: true,
@@ -30,7 +36,7 @@ $(document).ready(function(){
         items: 5,
         loop: true,
         autoplay: true,
-        nav:false,
+        nav: false,
         margin: 30,
         autoplayTimeout: 2000,
         autoplayHoverPause: true,
@@ -47,71 +53,71 @@ $(document).ready(function(){
         }
     })
 
-// accardion starts
-$(".head").click(function(ev){
-    $(this).parent().find(".body").slideToggle();
-    $(this).find("i").toggleClass("fa-minus");
-       $(".head").not($(this)).parent().find(".body").slideUp();
-       $(".head").not($(this)).find("i").removeClass("fa-minus").addClass("fa-plus");
-})
-// accardion ends
-// scroll top starts
-$('.daire').click(function(){
-    $('html, body').animate({ scrollTop: 0 }, "slow")
-    return false;
-})
+    // accardion starts
+    $(".head").click(function (ev) {
+        $(this).parent().find(".body").slideToggle();
+        $(this).find("i").toggleClass("fa-minus");
+        $(".head").not($(this)).parent().find(".body").slideUp();
+        $(".head").not($(this)).find("i").removeClass("fa-minus").addClass("fa-plus");
+    })
+    // accardion ends
+    // scroll top starts
+    $('.daire').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, "slow")
+        return false;
+    })
 
-$(window).scroll(function () {
-    scroll = $(window).scrollTop();
-    console.log(scroll)
-    if (scroll >= 500) {
-        $(".daire").css({
-            "position": "fixed",
-            "bottom": 40,
-            "right": 10
-        });
-    } else {
-        $(".daire").css({
-            "position": "absolute",
-            "right": "10px",
-            "bottom": "-500px"
-        });
-    }
-});
-// scroll top ends
-$(".resp-menu li").click(function () {
-    var scroll_number = $(this).attr("scroll");
-    var scroll_position = $("section[scroll=" + scroll_number + "]").offset().top;
-
-    $('html, body').animate({ scrollTop: scroll_position }, "slow")
-    return false;
-})
-   // menu navigation starrts
- $(".menu-items button").click(function () {
-    var scroll_number = $(this).attr("scroll");
-    var scroll_position = $("section[scroll=" + scroll_number + "]").offset().top;
-
-    $('html, body').animate({ scrollTop: scroll_position }, "slow")
-    return false;
-})
-// menu navigation ends
-
-// change menu item color when scroll starts
-var menu_legth = $(".menu-items button").length;
-console.log(menu_legth)
     $(window).scroll(function () {
         scroll = $(window).scrollTop();
-        for (var i=0;i<menu_legth-1;i++){
-            var scroll_position = $("section[scroll="+i+"]").offset().top
-            if (scroll >= scroll_position-10) {
-                $("button[scroll="+i+"]").addClass("active");
-                $("button[scroll="+i+"]").siblings().removeClass("active");
+        console.log(scroll)
+        if (scroll >= 500) {
+            $(".daire").css({
+                "position": "fixed",
+                "bottom": 40,
+                "right": 10
+            });
+        } else {
+            $(".daire").css({
+                "position": "absolute",
+                "right": "10px",
+                "bottom": "-500px"
+            });
+        }
+    });
+    // scroll top ends
+    $(".resp-menu li").click(function () {
+        var scroll_number = $(this).attr("scroll");
+        var scroll_position = $("section[scroll=" + scroll_number + "]").offset().top;
+
+        $('html, body').animate({ scrollTop: scroll_position }, "slow")
+        return false;
+    })
+    // menu navigation starrts
+    $(".menu-items button").click(function () {
+        var scroll_number = $(this).attr("scroll");
+        var scroll_position = $("section[scroll=" + scroll_number + "]").offset().top;
+
+        $('html, body').animate({ scrollTop: scroll_position }, "slow")
+        return false;
+    })
+    // menu navigation ends
+
+    // change menu item color when scroll starts
+    var menu_legth = $(".menu-items button").length;
+    console.log(menu_legth)
+    $(window).scroll(function () {
+        scroll = $(window).scrollTop();
+        for (var i = 0; i < menu_legth - 1; i++) {
+            var scroll_position = $("section[scroll=" + i + "]").offset().top
+            if (scroll >= scroll_position - 10) {
+                $("button[scroll=" + i + "]").addClass("active");
+                $("button[scroll=" + i + "]").siblings().removeClass("active");
             }
         }
     });
     // change menu item color when scroll ends
 
-    
+
 });
 var swiper = new Swiper('.swiper-container', {
     effect: 'coverflow',
@@ -119,14 +125,14 @@ var swiper = new Swiper('.swiper-container', {
     centeredSlides: true,
     slidesPerView: 'auto',
     coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows : true,
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
     },
     pagination: {
-      el: '.swiper-pagination',
+        el: '.swiper-pagination',
     },
-  });
+});
 
